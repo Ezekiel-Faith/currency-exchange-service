5ffef564107d739d707e2f7e0ec3b6448dd5391e
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-10-03 05:37:38 +0100

    commit

---
+++ src/main/java/com/in28minutes/microservices/currency_cexchange_service/CurrencyCexchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currency_cexchange_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCexchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCexchangeServiceApplication.class, args);
+	}
+
+}
