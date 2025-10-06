563ec56f60c388cb9c7c75395f02827ec84ebda5
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-10-06 12:00:03 +0100

    created a hard coded currency excahnge service

---
+++ src/main/java/com/in28minutes/microservices/currency_cexchange_service/CurrencyExchangeController.java
@@ -0,0 +1,17 @@
+package com.in28minutes.microservices.currency_cexchange_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from, @PathVariable String to) {
+        return new CurrencyExchange(1000L, "USD", "INR", BigDecimal.valueOf(50));
+    }
+}
