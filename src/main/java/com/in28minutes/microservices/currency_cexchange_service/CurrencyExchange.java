4daa3ec9b8127985d758547acf24b3482ec1c8d5
Ezekiel-Faith <ezekielfaith2000@gmail.com>
2025-10-06 13:22:54 +0100

    configuring jpa and initializing data

--- src/main/java/com/in28minutes/microservices/currency_cexchange_service/CurrencyExchange.java
+++ src/main/java/com/in28minutes/microservices/currency_cexchange_service/CurrencyExchange.java
@@ -1,11 +1,21 @@
 package com.in28minutes.microservices.currency_cexchange_service;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
